e32d0147c3050eeac833c6232fce2c2572e5b6d5
Michael Rae <mdrae0@gmail.com>
2020-04-29 17:37:42 -0500

    Adds third-module as a submodule

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const { sum, minus, powerOf} = require('./second-module/math.js')
 console.log(sum(2,10))
 console.log(minus(2,10))
 console.log(powerOf(2,10))
+
+const { concat } = require('./third-module/string.js')
+
+console.log(concat('hello ', 'submodules'))
